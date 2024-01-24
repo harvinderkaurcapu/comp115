da0e39602d39f2f359f9d2f8c6a193c544877050
harvinderkaurcapu <156242994+harvinderkaurcapu@users.noreply.github.com>
2024-01-24 12:11:41 -0800

    Lab1_harvinder.python

--- lab1_harvinder.py
+++ lab1_harvinder.py
@@ -43,7 +43,7 @@ while x < 10:
 print("There is a rectangle, of which one side is 3 and the other side is 4.") # This line prints a string.
 side_1 = 3
 side_2 = 4
-print(f"I think the area of the rectangle is: {side_1 + side_2}.") # This line prints a f-string, including variables in a string.
+print(f"I think the area of the rectangle is: {side_1 * side_2}.") # This line prints a f-string, including variables in a string.
 
 
 #---------------------------------------------------------
